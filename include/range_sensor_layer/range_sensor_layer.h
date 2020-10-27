04fc578d8ad9286d1bac732499c6418284caa5e5
lihuanhuan <13121837607@163.com>
2020-10-27 17:26:27 +0800

    add code

---
+++ include/range_sensor_layer/range_sensor_layer.h
@@ -0,0 +1,134 @@
+// Copyright 2018 David V. Lu!!
+#ifndef RANGE_SENSOR_LAYER_RANGE_SENSOR_LAYER_H_
+#define RANGE_SENSOR_LAYER_RANGE_SENSOR_LAYER_H_
+#include <ros/ros.h>
+#include <costmap_2d/costmap_layer.h>
+#include <costmap_2d/layered_costmap.h>
+#include <sensor_msgs/Range.h>
+#include <range_sensor_layer/RangeSensorLayerConfig.h>
+#include <dynamic_reconfigure/server.h>
+#include <list>
+#include <string>
+#include <vector>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <utility>
+
+namespace range_sensor_layer
+{
+
+class RangeSensorLayer : public costmap_2d::CostmapLayer
+{
+public:
+  enum InputSensorType
+  {
+    VARIABLE,
+    FIXED,
+    ALL
+  };
+
+  struct MarkerPoints
+  {
+    ros::Time marker_time;
+    std::vector< std::pair<double,double> > points;
+  };
+
+  RangeSensorLayer();
+
+  /*****************************
+   * 这在initialize（）的末尾被调用。 重写以实现特定于子类的初始化。
+   * 调用时，tf_，name_和layered_costmap_都将被设置。
+   ****************************/
+  virtual void onInitialize();
+
+  /*****************************
+   * 由LayeredCostmap调用，
+   * 以轮询此插件其需要更新多少成本图。
+   * 每层都可以增加此边界的大小。
+ ******************************/
+  virtual void updateBounds(double robot_x, double robot_y, double robot_yaw,
+                            double* min_x, double* min_y, double* max_x, double* max_y);
+
+  /******************************
+   * 实际上仅在UpdateBounds（）期间计算的范围内更新基础成本图。
+  *******************************/
+  virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+  virtual void reset();
+
+  /******************************
+   * 停止发布者。
+   ******************************/
+  virtual void deactivate();
+
+  /******************************
+   * 如果发布者已被停止，请重新启动它们。
+   *****************************/
+  virtual void activate();
+
+private:
+  void reconfigureCB(range_sensor_layer::RangeSensorLayerConfig &config, uint32_t level);
+
+  void bufferIncomingRangeMsg(const sensor_msgs::RangeConstPtr& range_message);
+  void processRangeMsg(sensor_msgs::Range& range_message);
+  void processFixedRangeMsg(sensor_msgs::Range& range_message);
+  void processVariableRangeMsg(sensor_msgs::Range& range_message);
+
+  void resetRange();
+  void updateCostmap();
+  void updateCostmap(sensor_msgs::Range& range_message, bool clear_sensor_cone);
+
+  double gamma(double theta);
+  double delta(double phi);
+  double sensor_model(double r, double phi, double theta);
+
+  void get_deltas(double angle, double *dx, double *dy);
+  void update_cell(double ox, double oy, double ot, double r, double nx, double ny, bool clear);
+
+  double to_prob(unsigned char c)
+  {
+    return static_cast<double>(c) / costmap_2d::LETHAL_OBSTACLE;
+  }
+  unsigned char to_cost(double p)
+  {
+    return static_cast<unsigned char>(p * costmap_2d::LETHAL_OBSTACLE);
+  }
+
+  boost::function<void(sensor_msgs::Range& range_message)> processRangeMessageFunc_;
+  boost::mutex range_message_mutex_;
+  std::list<sensor_msgs::Range> range_msgs_buffer_;
+
+  double max_angle_, phi_v_;
+  double inflate_cone_;
+  std::string global_frame_;
+
+  double clear_threshold_, mark_threshold_;
+  bool clear_on_max_reading_;
+
+  double no_readings_timeout_;
+  ros::Time last_reading_time_;
+  unsigned int buffered_readings_;
+  std::vector<ros::Subscriber> range_subs_;
+  double min_x_, min_y_, max_x_, max_y_;
+
+  void clearHistoryObs();
+  boost::mutex clear_mutex_;
+  std::vector< MarkerPoints > last_marker_points_buffer_;
+  double set_max_range_;
+  double set_min_range_;
+  double set_angle_;
+  double marker_duration_;
+
+  dynamic_reconfigure::Server<range_sensor_layer::RangeSensorLayerConfig> *dsrv_;
+
+
+  float area(int x1, int y1, int x2, int y2, int x3, int y3)
+  {
+    return fabs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2.0);
+  };
+
+  int orient2d(int Ax, int Ay, int Bx, int By, int Cx, int Cy)
+  {
+    return (Bx - Ax) * (Cy - Ay) - (By - Ay) * (Cx - Ax);
+  };
+};
+}  // namespace range_sensor_layer
+#endif  // RANGE_SENSOR_LAYER_RANGE_SENSOR_LAYER_H
